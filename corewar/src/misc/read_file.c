b93ac97eec9786110cfe30fadc97d0963a605e07
LouisBassagal <louis.bassagal@epitech.eu>
2022-05-10 17:29:50 +0200

    [ADD] Adding read_file.c

---
+++ corewar/src/misc/read_file.c
@@ -0,0 +1,24 @@
+/*
+** EPITECH PROJECT, 2022
+** Corewar [WSL: Manjaro]
+** File description:
+** read_file
+*/
+
+#include <fcntl.h>
+#include <unistd.h>
+
+char *read_file(char *filepath)
+{
+    int fd = open(filepath, O_RDONLY);
+    char *buff = malloc(1);
+    int offset = 0;
+    int len;
+
+    while ((len = read(fd, buff + offset, 1)) > 0) {
+        offset += 1;
+        buff = realloc(buff, offset + 1);
+    }
+    buff[offset + 1] = '\0';
+    close(fd);
+}
