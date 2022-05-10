86c52b62a3eff07f5fd27de64bea09f7689317e3
LouisBassagal <louis.bassagal@epitech.eu>
2022-05-10 17:49:36 +0200

    [FIX] Pushing again create_vm.c

---
+++ corewar/src/create_vm/create_vm.c
@@ -0,0 +1,25 @@
+/*
+** EPITECH PROJECT, 2022
+** Corewar [WSL: Manjaro]
+** File description:
+** create_corewar_struct
+*/
+
+#include <stdlib.h>
+#include "corewar.h"
+#include "my.h"
+
+corewar_t *create_vm(void)
+{
+    corewar_t *corewar = malloc(sizeof(corewar_t));
+
+    if (!corewar)
+        return NULL;
+    corewar->warrior_list.head = NULL;
+    corewar->warrior_list.nbr_of_warriors = 0;
+    corewar->nbr_cycles = 0;
+    my_memset(corewar->memory, MEM_SIZE, '\0');
+    my_memset(corewar->old_memory, MEM_SIZE, '\0');
+    corewar->warrior_list.id_of_warriors = 0;
+    return corewar;
+}
