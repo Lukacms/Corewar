d76ca560ae0e703388e51cff759012e318955421
Samuel Florentin <samuel.florentin@epitech.eu>
2022-05-18 10:13:28 +0200

    [ADD] function who read a file and return the size of the file

--- corewar/src/corewar.c
+++ corewar/src/corewar.c
@@ -12,9 +12,10 @@ int launch(int argc, char * const argv[])
 {
     corewar_t *corewar = create_vm();
 
+    if (!corewar)
+        return FAILURE;
     corewar->params = malloc(sizeof(parameters_t));
-    if (!corewar || arg_handling(argc, argv, corewar) != SUCCESS
-        || !corewar->params)
+    if (arg_handling(argc, argv, corewar) != SUCCESS || !corewar->params)
         return FAILURE;
     generate_warriors(corewar, argc, argv);
     return SUCCESS;
